ed39f7d0a868cf0a5a6d881642c6cb92e0fd6317
Rodrigo Duarte <rodrigo.terminus@gmail.com>
2016-12-21 00:22:44 -0200

    Fix loading spinner kepping after export

--- web/assets/js/exporter.js
+++ web/assets/js/exporter.js
@@ -1,5 +1,9 @@
 var exporter = {
     get: function(format) {
+        setTimeout(function() {
+            between.loading.hide();
+        }, 2000);
+        
         $('#export-data').val(JSON.stringify(exporter.getData()));
         $('#form-export')
             .attr('action', Routing.generate('export', { format: format }))
